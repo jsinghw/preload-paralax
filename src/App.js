9843e963446e6794ed2bdca671d69c910c246da2
Jaspal Singh <jsinghw94@gmail.com>
2020-11-06 23:07:55 -0500

    preloading font with @font-face and pre-loading images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import img1 from './assets/img/img1.jpeg'
+import img2 from './assets/img/img2.jpeg'
+import img3 from './assets/img/img3.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            imageIsReady: false,
+            pictures: [img1, img2, img3]
+        }
+    }
+    componentDidMount() {
+        this.state.pictures.forEach((picture) => {
+            const img = new Image();
+            img.src = picture.fileName;
+        });
+        this.setState({ imageIsReady: true })
+    }
+
+    render(){
+        const { imageIsReady } = this.state;
+        const { pictures } = this.state
+        if (!imageIsReady) {
+            return <div>Loading image...</div>; // or just return null if you want nothing to be rendered.
+        }else{
+            return (
+                <div className="App">
+                    <div>Here are 3 images</div>
+                    <img src={img1} alt="flower heart" />
+                    <img src={img2} alt="man stares into void"/>
+                    <img src={img3} alt="man walks the streets of cyberpunk city"/>
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
